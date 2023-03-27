b8255f67283dcb028d7ee7cad82f421c27a3205a
alper <leyalp77@gmail.com>
2023-03-27 21:08:32 +0300

    Phonebook Terraform

---
+++ tf-files/user-data.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+pip3 install flask_mysql
+yum install git -y
+TOKEN=${user-data-git-token}
+USER=${user-data-git-name}
+cd /home/ec2-user && git clone https://$TOKEN@github.com/$USER/phonebook.git
+python3 /home/ec2-user/phonebook/phonebook-app.py
---
+++ tf-files/var.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "james-clarusway"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "clarus"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us."
+}
+
