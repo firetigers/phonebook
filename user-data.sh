14f02febc78ae14a8f6c96f1d8e0fd2b6ed12f11
firetigers <117107965+firetigers@users.noreply.github.com>
2023-12-10 16:57:15 +0300

    Phonebook

--- user-data.sh
+++ user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="XXXXXXXXXXXX"
+TOKEN="XXXXXXXXXXXX" # Buraya kendi GitHub tokenınızı yazınız.
 cd /home/ec2-user && git clone https://$TOKEN@github.com/<account-name>/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
